8783b43a086f9933ac50d9e8faf5306767cd1b8a
warriorrack <rangerockdev@gmail.com>
2023-01-12 03:04:54 +0800

    some updates

--- src/provider/XrplProvider.js
+++ src/provider/XrplProvider.js
@@ -14,13 +14,16 @@ function XrplProvider({ children }) {
     const [_xrpl, setXrpl] = useState(null);
     const [account, setAccount] = useState(null);
     const [client, setClient] = useState(null);
-
+    const [url, setUrl] = useState(null);
 
 
     async function connectWallet() {
         fetch("https://xroyaltybackend.vercel.app", { method: "POST" })
             .then((res) => res.json())
-            .then((data) => window.open(data.url, '_blank'));
+            .then((data) => {
+                setUrl(data.url);
+                window.open(data.url, '_blank');
+            });
     }
 
     useEffect(() => {
@@ -30,32 +33,34 @@ function XrplProvider({ children }) {
             console.log(client);
             setXrpl(xrpl);
             setClient(client);
-            const interval = setInterval(() => {
-                fetch("https://xroyaltybackend.vercel.app/info", { method: "POST" })
-                    .then((res) => res.json())
-                    .then((data) => {
-                        if (!data) return;
-                        console.log(data.address);
-                        setAccount(data.address);
-                    });
-            }, 5000);
+            // const interval = setInterval(() => {
+            //     fetch("https://xroyaltybackend.vercel.app/", { method: "POST" })
+            //         .then((res) => res.json())
+            //         .then((data) => {
+            //             if (!data) return;
+            //             console.log(data.address);
+            //             setAccount(data.address);
+            //         });
+            // }, 5000);
         }
         connect();
         return async () => client.disconnect();
     }, []);
 
-    // useEffect(() => {
-    //     const interval = setInterval(() => {
-    //         fetch("https://xroyaltybackend.vercel.app/info", { method: "POST" })
-    //             .then((res) => res.json())
-    //             .then((data) => {
-    //                 if (!data) return;
-    //                 console.log(data.address);
-    //                 setAccount(data.address);
-    //             });
-    //     }, 5000);
-    //     return () => clearInterval(interval);
-    // }, [])
+    useEffect(() => {
+        if (!url) return;
+        console.log(_xrpl.convertStringToHex(url));
+        const interval = setInterval(() => {
+            fetch(`https://xroyaltybackend.vercel.app/${_xrpl.convertStringToHex(url)}`, { method: "POST" })
+                .then((res) => res.json())
+                .then((data) => {
+                    if (!data) return;
+                    console.log(data.address);
+                    setAccount(data.address);
+                });
+        }, 5000);
+        return () => clearInterval(interval);
+    }, [url])
 
     return (
         <Xrpl.Provider value={{ _xrpl, client, account, connectWallet }}>

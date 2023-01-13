2ed6bb7c43861d40339548ee6a94043be15fd145
warriorrack <rangerockdev@gmail.com>
2023-01-13 23:33:22 +0800

    added link to token id

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,44 +3,44 @@ import { Xrpl } from '../provider/XrplProvider';
 
 function View() {
 
-    const { _xrpl, client, account } = useContext(Xrpl);
-    const [nfts, setNfts] = useState([]);
-    const [nftAmount, setNftAmount] = useState("");
+  const { _xrpl, client, account } = useContext(Xrpl);
+  const [nfts, setNfts] = useState([]);
+  const [nftAmount, setNftAmount] = useState("");
 
-    useEffect(() => {
-        async function getNfts() {
-            if (!client) return;
-            if (!account) {
-                // alert("Please connect wallet");
-                return;
-            }
-            console.log(client);
-            const { result: { account_nfts } } = await client.request({
-                method: "account_nfts",
-                account: account
-            });
-            setNftAmount(account_nfts.length);
-            setNfts([]);
-            account_nfts?.forEach((item) => {
-                const tokenUri = _xrpl.convertHexToString(item.URI);
-                fetch(tokenUri).then((response) => response.json()).then(data => setNfts(p => [...p, { ...data, tokenID: item.NFTokenID }]));
-            });
-        }
-        getNfts();
-    }, [client, account]);
+  useEffect(() => {
+    async function getNfts() {
+      if (!client) return;
+      if (!account) {
+        // alert("Please connect wallet");
+        return;
+      }
+      console.log(client);
+      const { result: { account_nfts } } = await client.request({
+        method: "account_nfts",
+        account: account
+      });
+      setNftAmount(account_nfts.length);
+      setNfts([]);
+      account_nfts?.forEach((item) => {
+        const tokenUri = _xrpl.convertHexToString(item.URI);
+        fetch(tokenUri).then((response) => response.json()).then(data => setNfts(p => [...p, { ...data, tokenID: item.NFTokenID }]));
+      });
+    }
+    getNfts();
+  }, [client, account]);
 
-    return (
-        <div id="home">
-            <div className="title">Your NFTs{account ? `(${nftAmount})`: null}</div>
-            <div className="view">
-                {
-                    account
-                        ? nfts.length
-                            ? nfts.map((item, index) => (
+  return (
+    <div id="home">
+      <div className="title">Your NFTs{account ? `(${nftAmount})` : null}</div>
+      <div className="view">
+        {
+          account
+            ? nfts.length
+              ? nfts.map((item, index) => (
                                 <div className="card" key={index}>
                                     <div className="name">{item.name}</div>
                                     <div className="description">{item.description}</div>
-                                    <div className="id">ID: {item.tokenID}</div>
+                                    <a className="id" href={`https://xrpscan.com/nft/${item.tokenID}`}>ID: {item.tokenID}</a>
                                     <div className="content">
                                         <img src={item.image} />
                                         <div className="attributes">
@@ -56,12 +56,12 @@ function View() {
                                         </div>
                                     </div>
                                 </div>
-                            ))
-                            : <div className="no-nft">You don't have any NFTs!</div>
-                        : <div className="loading">Wallet not connected</div>
+      ))
+      : <div className="no-nft">You don't have any NFTs!</div>
+      : <div className="loading">Wallet not connected</div>
                 }
-            </div>
-        </div>
+    </div>
+        </div >
     )
 }
 

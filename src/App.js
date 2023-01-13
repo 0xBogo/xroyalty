dde4a10fe1efe4749a39c9fe999e67f710cb1113
CC-Peter <shnj1902@163.com>
2023-01-13 16:14:50 +0800

    updated view page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import Header from "./layout/Header";
 import './sass/styles.scss';
-import View from './pages/View';
+// import View from './pages/View';
 
 function App() {
 
@@ -11,7 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route index element={<Home />} />
-        <Route path="view" element={<View />} />
+        {/* <Route path="view" element={<View />} /> */}
       </Routes>
     </div>
   );
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -2,9 +2,23 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import { Xrpl } from '../provider/XrplProvider';
 import logo from "../assets/img/logo_word_white.png";
+import close from "../assets/img/close.svg";
 
 function Header() {
-  const { connectWallet, account } = useContext(Xrpl);
+  const { connectWallet, disconnectWallet, account } = useContext(Xrpl);
+  const [isBoxOpened, setIsBoxOpened] = useState(false);
+
+  const openBox = () => {
+    setIsBoxOpened(true);
+  }
+
+  const closeBox = () => {
+    setIsBoxOpened(false);
+  }
+
+  useEffect(() => {
+    closeBox();
+  }, [account])
 
   return (
     <div id="header">
@@ -13,16 +27,36 @@ function Header() {
           <img src={logo} />
           {/* <div>Xroyalty NFT</div> */}
         </a>
-        <div className="links">
+        {/* <div className="links">
           <Link to="/">Mint</Link>
           <Link to="/view">View</Link>
-        </div>
+        </div> */}
         <div className="connect-btn">
-          <button onClick={connectWallet}>
-            {account? account.slice(0, 3) + "..." + account.slice(-3): "Connect"}
-          </button>
+          {
+            account
+              ? <button onClick={openBox}>{account.slice(0, 3) + "..." + account.slice(-3)}</button>
+              : <button onClick={connectWallet}>Connect</button>
+          }
         </div>
       </div>
+      {
+        isBoxOpened &&
+        <div className="msg-box-container">
+          <div className="msg-box">
+            <div className="top">
+              <div className="title">Account Details</div>
+              <button onClick={closeBox}><img src={close} /></button>
+            </div>
+            <div className="main">
+              {account}
+            </div>
+            <div className="bottom">
+              <button className="change-btn" onClick={connectWallet}>Change Wallet</button>
+              <button className="disconnect-btn" onClick={disconnectWallet}>Disconnect</button>
+            </div>
+          </div>
+        </div>
+      }
     </div>
   )
 }
